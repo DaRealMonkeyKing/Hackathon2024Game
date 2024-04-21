0eeee023e9e2fcc96352460fe32471e5c6c2ccab
DaRealMonkeyKing <niel20060709@gmail.com>
2024-04-21 16:43:00 -0400

    Added A Dash
    Dashes towards mouse with limited length
    Control: Space

--- Scripts/PlayerCharacter.cs
+++ Scripts/PlayerCharacter.cs
@@ -1,11 +1,16 @@
 using Godot;
 using System;
+using System.Numerics;
+
+namespace Godot;
 
 public partial class PlayerCharacter : CharacterBody2D
 {
 	private Vector2 currentVelocity;
 	[Export]
 	private int speed = 1000;
+	private int dash_length = 500;
+	private Vector2 mouse_loc_from_player, mouse_loc;
 
     public override void _PhysicsProcess(double delta)
     {
@@ -17,11 +22,24 @@ public partial class PlayerCharacter : CharacterBody2D
 	private void getInput() {
 		currentVelocity = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 		currentVelocity *= speed;
+		
+		mouse_loc = GetViewport().GetMousePosition(); // get mouse position based off viewport
+		mouse_loc_from_player = mouse_loc - Position; // get mouse distance from player
 	}
 
 	private void playerMovement() {
 		getInput();
+		blink();
 		Velocity = currentVelocity;
 		MoveAndSlide();
     }
+
+	private void blink() {
+		if (Input.IsActionJustReleased("space")) {
+			if (mouse_loc_from_player.Length() > dash_length)
+				Position += mouse_loc_from_player.LimitLength(dash_length);
+			else
+				Position = mouse_loc;
+		}
+	}
 }

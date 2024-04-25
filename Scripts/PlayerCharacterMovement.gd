5faff6a3f379fd939ed44f17dbcbdf3ef1ddcaaf
Ryan-Xue2 <ryan.xue2@gmail.com>
2024-04-24 23:30:26 -0400

    added dash, needs a little tweaking but overall good

--- Scripts/PlayerCharacterMovement.gd
+++ Scripts/PlayerCharacterMovement.gd
@@ -6,15 +6,17 @@ extends CharacterBody2D
 @export var FRICTION = 1000
 
 var input = Vector2.ZERO
-
+ 
 
 func _physics_process(delta):
 	player_movement(delta)
+
 
 func get_input():
 	input.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
 	input.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
 	return input.normalized()
+
 
 func player_movement(delta):
 	input = get_input()
